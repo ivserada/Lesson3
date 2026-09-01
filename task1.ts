2dfda17169315154035e1021b77f3674fb762068
ivserada <ivserada@gmail.com>
2026-09-01 20:50:32 +0300

    Fix: task1

--- task1.ts
+++ task1.ts
@@ -1,5 +1,8 @@
 const age: number = 23;
-
+const days: number = age * 365;
+const hours: number = days * 24;
+const minutes: number = hours * 60;
+const seconds: number = minutes * 60;
 //добавить минуты
 
-console.log(`Мне ${age * 365} дней ${age * 365 * 24} часов и ${age * 365 * 24 * 3600} секунд`);
+console.log(`Мне ${age * 365} дней ${hours} часов ${minutes} минут и ${seconds} секунд`);
